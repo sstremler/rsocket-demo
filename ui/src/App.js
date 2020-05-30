a85f63eb0e5012fa6e015d863c379bf28964b120
sstremler <stremler.sz@gmail.com>
2020-05-31 00:52:08 +0200

    add cancel and disconnect feature

--- ui/src/App.js
+++ ui/src/App.js
@@ -7,36 +7,39 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 
 class App extends Component {
-    clientStreamSubscription;
-    serverStreamSubscription;
+    requestChannelClientSubscription;
+    requestChannelServerSubscription;
+    requestStreamSubscription;
 
     constructor(props) {
         super(props);
         this.state = {
+            connected: false,
+            streamInProgress: false,
+            channelInProgress: false,
             address: 'ws://localhost:7000',
             log: ''
         }
 
         this.handleConnect = this.handleConnect.bind(this);
-        this.handleDisconnect = this.handleDisconnect.bind(this);
         this.handleRequestResponse = this.handleRequestResponse.bind(this);
         this.handleRequestStream = this.handleRequestStream.bind(this);
         this.handleFireAndForget = this.handleFireAndForget.bind(this);
         this.handleRequestChannel = this.handleRequestChannel.bind(this);
-        this.handleCancelChannel = this.handleCancelChannel.bind(this);
     }
 
     handleConnect(event) {
-        this.client = new Client(this.state.address);
-        this.client.connect().then(sub => {
-            this.appendLog('Connected to ' + this.state.address);
-        });
-    }
-
-    handleDisconnect(event) {
-        console.log('disconnect click');
-        // connection.close();
-        this.client.disconnect();
+        if (!this.state.connected) {
+            this.client = new Client(this.state.address);
+            this.client.connect().then(sub => {
+                this.setState({connected: true});
+                this.appendLog('Connected to ' + this.state.address);
+            });
+        } else {
+            this.client.disconnect();
+            this.setState({connected: false});
+            this.appendLog('Disconnected from ' + this.state.address);
+        }
     }
 
     handleRequestResponse(event) {
@@ -54,98 +57,106 @@ class App extends Component {
     }
 
     handleRequestStream(event) {
-        let subscription;
-        let requestedMsg = 10;
-        let processedMsg = 0;
-        let msg = new Message('client', 'request');
-        this.appendLog('REQUEST STREAM, request: ' + msg.toString());
+        if(!this.state.streamInProgress) {
+            let requestedMsg = 10;
+            let processedMsg = 0;
+            let msg = new Message('client', 'request');
+            this.appendLog('REQUEST STREAM, request: ' + msg.toString());
 
-        this.client.requestStream(msg).subscribe({
-            onSubscribe: sub => {
-                this.appendLog('REQUEST STREAM: subscribed to stream');
-                subscription = sub;
-                this.appendLog('REQUEST STREAM: request '+ requestedMsg +' messages');
-                subscription.request(requestedMsg);
-            },
-            onError: error => {
-                this.appendLog('REQUEST STREAM: error occurred: ', JSON.stringify(error));
-            },
-            onNext: msg => {
-                this.appendLog('REQUEST STREAM: new message arrived: ', new Message().toObject(msg.data).toString());
-                processedMsg++;
+            this.client.requestStream(msg).subscribe({
+                onSubscribe: sub => {
+                    this.appendLog('REQUEST STREAM: subscribed to stream');
+                    this.requestStreamSubscription = sub;
+                    this.appendLog('REQUEST STREAM: request ' + requestedMsg + ' messages');
+                    this.requestStreamSubscription.request(requestedMsg);
+                    this.setState({streamInProgress: true});
+                },
+                onError: error => {
+                    this.appendLog('REQUEST STREAM: error occurred: ' + JSON.stringify(error));
+                },
+                onNext: msg => {
+                    this.appendLog('REQUEST STREAM: new message arrived: ' + new Message().toObject(msg.data).toString());
+                    processedMsg++;
 
-                if (processedMsg >= requestedMsg) {
-                    this.appendLog('REQUEST STREAM: request '+ requestedMsg +' messages');
-                    subscription.request(requestedMsg);
-                    processedMsg = 0;
-                }
+                    if (processedMsg >= requestedMsg) {
+                        this.appendLog('REQUEST STREAM: request ' + requestedMsg + ' messages');
+                        this.requestStreamSubscription.request(requestedMsg);
+                        processedMsg = 0;
+                    }
 
-            },
-            onComplete: msg => {
-                this.appendLog('REQUEST STREAM: stream completed');
-            },
-        });
+                },
+                onComplete: msg => {
+                    this.appendLog('REQUEST STREAM: stream completed');
+                },
+            });
+        } else {
+            this.requestStreamSubscription.cancel();
+            this.setState({streamInProgress: false});
+            this.appendLog('REQUEST STREAM: stream cancelled');
+        }
     }
 
     handleRequestChannel(event) {
-        let index = 0;
-        let requestedMsg = 10;
-        let processedMsg = 0;
-        let cancelled = false;
+        if(!this.state.channelInProgress) {
+            let index = 0;
+            let requestedMsg = 10;
+            let processedMsg = 0;
+            let cancelled = false;
 
-        let flow = new Flowable(subscriber => {
-            this.clientStreamSubscription = subscriber;
-            this.clientStreamSubscription.onSubscribe({
-                cancel: () => {
-                    cancelled = true;
-                },
-                request: n => {
-                    this.appendLog('REQUEST CHANNEL: OUTBOUND: '+ n +' message(s) was/were requested by the server');
+            let flow = new Flowable(subscriber => {
+                this.requestChannelClientSubscription = subscriber;
+                this.requestChannelClientSubscription.onSubscribe({
+                    cancel: () => {
+                        cancelled = true;
+                    },
+                    request: n => {
+                        this.appendLog('REQUEST CHANNEL: OUTBOUND: ' + n + ' message(s) was/were requested by the server');
 
-                    let intervalID = setInterval(() => {
-                        if (n > 0 && !cancelled) {
-                            const msg = new Message('client', 'stream', index++);
-                            subscriber.onNext(msg);
-                            this.appendLog('REQUEST CHANNEL: OUTBOUND: new message sent: ' + msg.toString());
-                            n--;
-                        } else {
-                            window.clearInterval(intervalID);
-                        }
-                    }, 1000);
-                }
+                        let intervalID = setInterval(() => {
+                            if (n > 0 && !cancelled) {
+                                const msg = new Message('client', 'stream', index++);
+                                subscriber.onNext(msg);
+                                this.appendLog('REQUEST CHANNEL: OUTBOUND: new message sent: ' + msg.toString());
+                                n--;
+                            } else {
+                                window.clearInterval(intervalID);
+                            }
+                        }, 1000);
+                    }
+                });
             });
-        });
 
-        this.client.requestChannel(flow).subscribe({
-            onSubscribe: sub => {
-                this.appendLog('REQUEST CHANNEL: INBOUND: subscribed to stream');
-                this.serverStreamSubscription = sub;
-                this.serverStreamSubscription.request(requestedMsg);
-                this.appendLog('REQUEST CHANNEL: INBOUND: '+ requestedMsg +' message(s) was/were requested by the client');
-            },
-            onError: error => {
-                this.appendLog('REQUEST CHANNEL: INBOUND: error occurred:' + JSON.stringify(error));
-            },
-            onNext: msg => {
-                this.appendLog('REQUEST CHANNEL: INBOUND: new message arrived: ' + new Message().toObject(msg.data).toString());
-                processedMsg++;
+            this.client.requestChannel(flow).subscribe({
+                onSubscribe: sub => {
+                    this.appendLog('REQUEST CHANNEL: INBOUND: subscribed to stream');
+                    this.requestChannelServerSubscription = sub;
+                    this.requestChannelServerSubscription.request(requestedMsg);
+                    this.appendLog('REQUEST CHANNEL: INBOUND: ' + requestedMsg + ' message(s) was/were requested by the client');
+                    this.setState({channelInProgress: true});
+                },
+                onError: error => {
+                    this.appendLog('REQUEST CHANNEL: INBOUND: error occurred:' + JSON.stringify(error));
+                },
+                onNext: msg => {
+                    this.appendLog('REQUEST CHANNEL: INBOUND: new message arrived: ' + new Message().toObject(msg.data).toString());
+                    processedMsg++;
 
-                if (processedMsg >= requestedMsg) {
-                    this.serverStreamSubscription.request(requestedMsg);
-                    this.appendLog('REQUEST CHANNEL: INBOUND: '+ requestedMsg +' message(s) was/were requested by the client');
-                    processedMsg = 0;
-                }
-            },
-            onComplete: msg => {
-                console.log('REQUEST CHANNEL: INBOUND: stream completed')
-            },
-        });
-    }
-
-    handleCancelChannel(event) {
-        this.clientStreamSubscription._subscription.cancel();
-        this.serverStreamSubscription.cancel();
-        console.log('cancel channel');
+                    if (processedMsg >= requestedMsg) {
+                        this.requestChannelServerSubscription.request(requestedMsg);
+                        this.appendLog('REQUEST CHANNEL: INBOUND: ' + requestedMsg + ' message(s) was/were requested by the client');
+                        processedMsg = 0;
+                    }
+                },
+                onComplete: msg => {
+                    console.log('REQUEST CHANNEL: INBOUND: stream completed')
+                },
+            });
+        } else {
+            this.requestChannelClientSubscription._subscription.cancel();
+            this.requestChannelServerSubscription.cancel();
+            this.setState({channelInProgress: false});
+            this.appendLog('REQUEST CHANNEL: channel cancelled');
+        }
     }
 
     handleAddressChange(event) {
@@ -171,19 +182,18 @@ class App extends Component {
                     placeholder="ws://localhost:7000"
                     fullWidth
                     autoFocus
-                    required
                 />
                 <Button variant="contained" color="primary" className="connect-btn"
-                        onClick={this.handleConnect}>Connect</Button>
+                        onClick={this.handleConnect}>{this.state.connected === true ? 'Disconnect' : 'Connect'}</Button>
             </div>
             <div className="btn-container">
-                {/*<button onClick={this.handleDisconnect}>Disconnect</button>*/}
                 <Button variant="contained" color="primary" onClick={this.handleRequestResponse}>Request
                     response</Button>
                 <Button variant="contained" color="primary" onClick={this.handleFireAndForget}>Fire and forget</Button>
-                <Button variant="contained" color="primary" onClick={this.handleRequestStream}>Request stream</Button>
-                <Button variant="contained" color="primary" onClick={this.handleRequestChannel}>Request channel</Button>
-                {/*<Button variant="contained" color="primary" onClick={this.handleCancelChannel}>Cancel channel</Button>*/}
+                <Button variant="contained" color="primary"
+                        onClick={this.handleRequestStream}>{this.state.streamInProgress ? 'Cancel stream' : 'Request stream'}</Button>
+                <Button variant="contained" color="primary"
+                        onClick={this.handleRequestChannel}>{this.state.channelInProgress ? 'Cancel channel' : 'Request channel'}</Button>
             </div>
             <div className="messages-container">
                 <TextField
