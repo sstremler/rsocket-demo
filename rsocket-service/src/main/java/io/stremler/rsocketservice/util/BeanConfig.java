ab908ea683588d2eaa9543d6f8713610d5c62655
sstremler <stremler.sz@gmail.com>
2020-05-31 15:40:42 +0200

    delete unused beans

--- rsocket-service/src/main/java/io/stremler/rsocketservice/util/BeanConfig.java
+++ rsocket-service/src/main/java/io/stremler/rsocketservice/util/BeanConfig.java
@@ -1,31 +1,12 @@
 package io.stremler.rsocketservice.util;
 
 import io.rsocket.RSocketFactory;
-import io.stremler.rsocketservice.model.Message;
 import org.springframework.boot.rsocket.server.ServerRSocketFactoryProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import reactor.core.publisher.*;
-
-import java.time.Duration;
 
 @Configuration
 public class BeanConfig {
-
-    @Bean
-    public DirectProcessor<Message> unicastProcessor() {
-        return DirectProcessor.create();
-    }
-
-    @Bean
-    public Flux<Message> messageFlux(DirectProcessor<Message> processor) {
-        return processor.publish().autoConnect();
-    }
-
-    @Bean
-    public FluxSink<Message> messageFluxSink(DirectProcessor<Message> processor) {
-        return processor.sink();
-    }
 
     @Bean
     ServerRSocketFactoryProcessor serverRSocketFactoryProcessor() {
